4b37a516b71b9856a2450b4e9e1951e6c81b807c
metheok <studentinok@gmail.com>
2020-07-25 18:40:24 +0530

    designed premium account add section

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -57,31 +57,32 @@
       </div>
 
       <div class="followers">
-        
-          <div class="follow-heading">
-            <h4 class="follow-head">Followers</h4>
-            <p class="view-all">View All</p>
-          </div>
+        <div class="follow-heading">
+          <h4 class="follow-head">Followers</h4>
+          <p class="view-all">View All</p>
+        </div>
 
-          <div class="followers-wrapper">
-
+        <div class="followers-wrapper">
           <a class="followers" href="#">
             <img
               class="follower-img"
               src="https://i.pinimg.com/236x/2d/0a/c5/2d0ac5f5a8380ba790cb88845ea4484c.jpg"
-            /> <p>James May</p>
+            />
+            <p>James May</p>
           </a>
           <a class="followers" href="#">
             <img
               class="follower-img"
               src="https://i.pinimg.com/236x/d6/f8/14/d6f814dfb3a5d372fa8eab3558f35047.jpg"
-            /> <p>Paul Mcartney</p>
+            />
+            <p>Paul Mcartney</p>
           </a>
           <a class="followers" href="#">
             <img
               class="follower-img"
               src="https://i.pinimg.com/236x/74/f5/a0/74f5a0511beb735dae7350f1526ff215.jpg"
-          />  <p>Jhonny cash</p>
+            />
+            <p>Jhonny cash</p>
           </a>
         </div>
       </div>
@@ -177,12 +178,15 @@ export default {};
 }
 
 .banner {
-  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: white;
+  background-size: 100%;
+  background-position: center;
+  background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Globe_centered_in_the_Atlantic_Ocean_%28green_and_grey_globe_scheme%29.svg/1200px-Globe_centered_in_the_Atlantic_Ocean_%28green_and_grey_globe_scheme%29.svg.png);
+  padding: 60px 0;
   margin-top: 30px;
   border-radius: 20px;
 }
@@ -227,7 +231,9 @@ button:active {
 .follow-heading {
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
+
 .followers-wrapper {
   display: flex;
 }
@@ -245,7 +251,8 @@ button:active {
 a > p {
   position: relative;
   text-align: center;
-  background-color: #a0949b;
+  background-color: rgba(0, 0, 0, 0.22);
+
   font-size: 10px;
   color: whitesmoke;
   bottom: 35px;
